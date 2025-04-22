407414c1261c82e264276356fba77d9a2c6ffb44
gabwowce <gt1230112301@gmail.com>
2025-04-22 10:39:13 +0300

    updating
    done
    
    i
    
    iupdate

--- app/db/session.py
+++ app/db/session.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "mysql+pymysql://root:12301@localhost:3306/autorentdb"
-
+# Naudojama reali konfiguracija iš .env
+DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:12301@localhost:3306/autorentdb")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
